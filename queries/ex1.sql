7c6bf88b0e8427c3260aea31ef29e42d584f4b40
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2019-11-12 10:54:36 +0900

    答えを消した

--- queries/ex1.sql
+++ queries/ex1.sql
@@ -1,6 +1,3 @@
 # == Exercise 1 ==
 # '株式会社タニタ' さんのメールアドレスを求めよ。
 # この下の行にSQL文を書きましょう。
-SELECT email
-FROM user
-WHERE name = '株式会社タニタ'
--- queries/ex3.sql
+++ queries/ex3.sql
@@ -1,8 +1,3 @@
 # == Exercise 2 ==
 # 'SHARP シャープ株式会社' さんをフォローする人の表示名（name)を求めよ。
 # この下の行にSQL文を書きましょう。
-SELECT u2.name
-FROM user u, follow f, user u2 
-WHERE u.name = 'SHARP シャープ株式会社'
-  and f.followee_account = u.account
-  and f.follower_account = u2.account
