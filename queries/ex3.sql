54349dc48b9b0da0971be379db520a6e255afe3a
Chiemi Watanabe <chiemi627@users.noreply.github.com>
2019-11-13 18:26:04 +0900

    Update ex3.sql

--- queries/ex3.sql
+++ queries/ex3.sql
@@ -1,3 +1,8 @@
 # == Exercise 2 ==
 # 'SHARP シャープ株式会社' さんをフォローする人の表示名（name)を求めよ。
 # この下の行にSQL文を書きましょう。
+SELECT u2.name
+FROM user u1, user u2, follow f
+WHERE u1.name = 'SHARP シャープ株式会社'
+  and u1.account = f.followee_account
+  and u2.account = f.follower_account
