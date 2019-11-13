bd48d1002d47276a5370d8e668447a638901954f
Chiemi Watanabe <chiemi627@users.noreply.github.com>
2019-11-13 18:24:50 +0900

    Update ex2.sql

--- queries/ex2.sql
+++ queries/ex2.sql
@@ -1,3 +1,7 @@
 # == Exercise 2 ==
 # 'SHARP シャープ株式会社' さんをフォローする人のアカウント名を求めよ。
 # この下の行にSQL文を書きましょう。
+SELECT f.follower_account
+FROM follow f, user u
+WHERE f.followee_account = u.account
+  and u.name = 'SHARP シャープ株式会社'
